9528f757a2895e97d923eeb9392d00c4a304cb30
mikinori <1078sa08002@gmail.com>
2020-05-08 10:31:23 +0900

    22_質問集の質問投稿ページを実装

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,17 @@
 class QuestionsController < ApplicationController
   def index
     @questions = Question.all.order(created_at: "DESC")
+    @question = Question.new
   end
+
+  def create
+    Question.create(question_params)
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :detail)
+  end
+
 end
