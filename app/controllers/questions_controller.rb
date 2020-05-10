074d5b4817aa6fe16563e6ee5f0247e53523673c
mikinori <1078sa08002@gmail.com>
2020-05-10 20:46:49 +0900

    22_質問集の質問投稿ページを実装-修正2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,8 +7,9 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
     if @question.save
-      redirect_to question_path, notice: "質問を投稿しました。"
+      redirect_to questions_path, notice: "質問を投稿しました。"
     else
+      @questions = Question.all.order(created_at: "DESC")
       flash.now[:alert] = "質問の投稿に失敗しました"
       render :index
     end
