b56ec4eb671bf407c96b683af46bd72efa5d23bd
d-saori <60836449+d-saori@users.noreply.github.com>
2020-06-24 09:22:06 +0900

    create_action, toutes fix

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,5 +1,8 @@
 class SolutionsController < ApplicationController
   def create
     @solution = Solution.new(solution_params)
+    @solution.question_id = current_question.id
+    @solution.save
+    redirect_to quwestions_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
   root to: 'movies#index'
-  resources :questions, only: [:index, :create, :show]
+  resources :questions do
+    resources :solutions, only: [:create]
+  end
   resources :aws_texts, only: [:index, :show]
   resources :live_codings, only: [:index]
   resources :talks, only: [:index]
-  resources :solutions
 
 end
