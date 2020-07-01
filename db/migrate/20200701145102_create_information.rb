774e4a56372de89a0ea83bafbbb22ca89e640d4e
d-saori <60836449+d-saori@users.noreply.github.com>
2020-07-01 23:58:06 +0900

    マイグレーションファイル編集

--- db/migrate/20200701145102_create_information.rb
+++ db/migrate/20200701145102_create_information.rb
@@ -1,7 +1,9 @@
 class CreateInformation < ActiveRecord::Migration[6.0]
   def change
     create_table :information do |t|
-
+      t.string :category
+      t.string :title
+      t.string :url
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_133847) do
+ActiveRecord::Schema.define(version: 2020_07_01_145102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,14 @@ ActiveRecord::Schema.define(version: 2020_06_01_133847) do
   create_table "aws_texts", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "information", force: :cascade do |t|
+    t.string "category"
+    t.string "title"
+    t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
