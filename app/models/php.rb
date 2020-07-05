059c16f1820f07dab20e27aa63e7a8852a8aed39
mikinori <1078sa08002@gmail.com>
2020-07-05 19:53:03 +0900

    PHP講座ページのモデル、テーブル、バリデーションの作成、タイプミスの修正2回目

---
+++ app/models/php.rb
@@ -0,0 +1,4 @@
+class Php < ApplicationRecord
+  validates :title, presence: true
+  validates :url, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_05_045729) do
+ActiveRecord::Schema.define(version: 2020_07_05_104555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,13 @@ ActiveRecord::Schema.define(version: 2020_07_05_045729) do
   end
 
   create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "phps", force: :cascade do |t|
     t.string "title"
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
